a0702dc777395aecc01bd8851333f91a8ca0905a
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 16:33:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,4 +28,3 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select do { |element| element == valid_calls.any? }
 end
-end
